0143d5fc5d41963664ee0ec538fc0020b22f563e
ram <ramsankarthedeveloper@gmail.com>
2023-06-26 15:26:44 +0530

    resolve #1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils import find_packages,setup
-
+from setuptools import find_packages,setup
 from typing import List
-HYPEN_E_DOT='-e .' #""" the '-e .' refers to the setup.py as package(?)"""
+
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -10,22 +10,18 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name = "Question generation",
-    description = "nlp project",
-    long_description = """The project is to use classical NLP to generate questions from a given text.
-                    Update 1 will be generating questions based on Bloom's taxonomy""",
-    version = 0.0.1,
-    download_url ="",
+name='question generation',
+version='0.0.1',
+author='Ram sankar.p',
+author_email='ramsankarthedeveloper@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
-    author = "Ram sankar.P",
-    author_email = "RamSankarTheDeveloper@gmail.com",
-    
-
-    install_requires = get_requirements("requirements.txt"),
-    packages = find_packages()
-    )
+)
