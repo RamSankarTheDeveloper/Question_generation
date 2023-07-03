73c323f74f96e7584820edb2e5a556c71187df50
ram <ramsankarthedeveloper@gmail.com>
2023-07-03 13:38:51 +0530

    resolve #5

--- src/main.py
+++ src/main.py
@@ -21,19 +21,19 @@ class question:
                      ]  
    print("wh_word= ", self.wh_word[0])
 
- def subj_verb(self):
+ def subj_verb_aux(self):
    self.subj= self.prep_def.svo_selecter("nsubj")
    self.verb= self.prep_def.svo_selecter("ROOT")
-   print("subj, verb= ",self.subj, self.verb)
+   self.aux= self.aux_part(self.subj.text.lower() )
+   print("subj, verb, aux= ",self.subj,',', self.verb,',', self.aux[0])
 
-
-
-#  def aux_part(self):
-#    self.pos_tags= [nltk.pos_tag([token]) for token in word_tokenize("I like books very much because I like reading.")]
-#    print("post tags= ", self.pos_tags)
+ def aux_part(self, keyword):
+   if keyword in list(aux_dict.keys()):
+     return aux_dict[keyword]
+   #add nouns, including names
 
 
 
 
 question().wh_part()
-question().subj_verb()
+question().subj_verb_aux()
