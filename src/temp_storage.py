c34e33c333424af4a96e7178cdf8a00088862ac9
ram <ramsankarthedeveloper@gmail.com>
2023-07-10 17:10:07 +0530

    resolve #17

--- src/temp_storage.py
+++ src/temp_storage.py
@@ -1,18 +1,29 @@
 # config
-text = "him and I like action films since 2019"
+text = "him and I like action films because 2019 happened"
 
 
 # data
 # Different question types possible for each discourse marker
-trigger_words_dict = {'because': ['Why'], 'since': ['When', 'Why'], 'when': ['When'], 'although': ['Yes/No'], 'as a result': ['Why'],
-                      'for example': ['Give an example where'], 'for instance': ['Give an instance where'], 'to': ['Why'],
+trigger_words_dict = {'because': ['Why'], 'since': ['When'], 'when': ['When'], 
+                      #'although': ['do'], 'to': ['Why'],'for instance': ['Give an instance where'], 
+                      'for example': ['Give an example where'], 'as a result': ['Why'],
                       'meanwhile': ['What was happening simultaneously when'], 'thus': ['How can', 'Why did']}
 
 
-aux_dict = {'i': ['do'], 'we': ['do'], 'you': ['do'], 'they': [
-    'do'], 'he': ['does'], 'she': ['does'], 'it': ['does']}
+aux_dict = {'i': ['do'], 'we': ['do'], 'you': ['do'], 'they': ['do'], 'he': ['does'], 'she': ['does'], 'it': ['does']}
 
-sent_split = {'because': 0,'since':0}
+sent_split = {'because': 0,'since':0, 'when':0, 'as a result':1 , 'although': 0, 'meanwhile':1 , 'thus':1, 
+              #'as a result': 1 ,'for example': 0    ##space between words don't count in tokens
+                }
 
-"""wh:1, aux:2, subj:3, verb:4, rest_of_question:5"""
-qg_order= {'because':['wh','aux','rest','?'],'since':['trigger','wh','aux','rest','?']}
+
+qg_order= {'because':['wh','aux','rest','?'],'since':['trigger','wh','aux','rest','?'], 'when': ['wh','aux','rest','?'],
+             'for example': ['wh', 'rest','?'], 'meanwhile':['wh','rest','?'], 'thus': [], 'as a result':['wh', 'rest','?']
+               }
+
+
+
+"""
+since:['when', 'why']
+although:
+"""
